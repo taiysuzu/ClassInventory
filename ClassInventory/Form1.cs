6128221d63b31a185ee787af4a2860bd9bd61aae
Taiyo Suzuki <taiysuzu199@ed.amdsb.ca>
2021-09-22 10:30:10 -0400

    complete

--- ClassInventory/Form1.cs
+++ ClassInventory/Form1.cs
@@ -12,7 +12,7 @@ namespace ClassInventory
 {
     public partial class Form1 : Form
     {
-        // TODO - create a List to store all inventory objects
+        List<Player> playerInventory = new List<Player>();               // TODO - create a List to store all inventory objects
 
         public Form1()
         {
@@ -21,13 +21,22 @@ namespace ClassInventory
 
         private void addButton_Click(object sender, EventArgs e)
         {
-            // TODO - gather all information from screen 
+            string name, age, team, position;                           // TODO - gather all information from screen 
+            name = newNameInput.Text;
+            age = ageInput.Text;
+            team = teamInput.Text;
+            position = positionInput.Text;
 
-            // TODO - create object with gathered information
+            Player tempPlayer = new Player(name, age, team, position);      // TODO - create object with gathered information
 
-            // TODO - add object to global list
+            playerInventory.Add(tempPlayer);                // TODO - add object to global list
 
             // TODO - display message to indicate addition made
+            label1.Text = "";
+            label1.Text += tempPlayer.name + ", "
+                    + tempPlayer.age + ", "
+                    + tempPlayer.team + ", "
+                    + tempPlayer.position + " - Added\n";
         }
 
         private void removeButton_Click(object sender, EventArgs e)
@@ -35,10 +44,24 @@ namespace ClassInventory
             // This is to be completed in Part II. You will use 
             // Lambda Expressions.
             //---------------------------
+            Player removing = playerInventory.Find(x => x.name == $"{removeInput.Text}");
 
-            // TODO - if object is in list remove it
+            int index = playerInventory.FindIndex(n => n.name == $"{removeInput.Text}");
+            if (index >= 0)                        // TODO - if object is in list remove it
+            {
+                playerInventory.RemoveAt(index);
 
-            // TODO - display message to indicate deletion made
+                label1.Text = "";
+                label1.Text += removing.name + ", "
+                        + removing.age + ", "
+                        + removing.team + ", "
+                        + removing.position + " - Removed\n";            // TODO - display message to indicate deletion made
+            }
+            else
+            {
+                label1.Text = "";
+                label1.Text += "Player Not Found";
+            }
         }
 
         private void searchButton_Click(object sender, EventArgs e)
@@ -49,11 +72,35 @@ namespace ClassInventory
 
             // TODO - if object entered exists in list show it
             // TODO - else show not found message
+
+            Player searched = playerInventory.Find(x => x.name == $"{searchInput.Text}");
+
+            int index = playerInventory.FindIndex(n => n.name == $"{searchInput.Text}");
+            if (index >= 0)
+            {
+                label1.Text = "";
+                label1.Text += searched.name + ", "
+                        + searched.age + ", "
+                        + searched.team + ", "
+                        + searched.position + "\n";
+            }
+            else
+            {
+                label1.Text = "";
+                label1.Text += "Player Not Found";
+            }
         }
 
         private void showButton_Click(object sender, EventArgs e)
         {
             // TODO - show all objects in list. Use a foreach loop.
+            playerInventory = playerInventory.OrderBy(x => x.team).ThenBy(x => x.name).ToList();
+            label1.Text = "";
+            foreach (Player tempPlayer in playerInventory)
+            {
+                label1.Text += tempPlayer.name + ", " + tempPlayer.age + ", " + tempPlayer.team + ", " + tempPlayer.position + "\n";
+            }
+
         }
     }
 }
---
+++ ClassInventory/Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassInventory
+{
+    class Player
+    {
+        public string name, age, team, position;
+        public Player(string _name, string _age, string _team, string _position)
+        {
+            name = _name;
+            age = _age;
+            team = _team;
+            position = _position;
+        }
+    }
+}
